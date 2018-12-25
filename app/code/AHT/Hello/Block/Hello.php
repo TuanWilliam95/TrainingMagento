ae972e60f84006b0b395c4c8aa2d293210bce40c
TuanWilliam95 <tuanwilliam9x@gmail.com>
2018-12-25 10:38:58 +0700

    create first module in magento 2

---
+++ app/code/AHT/Hello/Block/Hello.php
@@ -0,0 +1,7 @@
+<?php 
+namespace AHT\Hello\Block;
+class Hello extends \Magento\Framework\View\Element\Template{
+	protected function _prepareLayout(){
+		return parent::_prepareLayout();
+	}
+}
---
+++ app/code/AHT/Hello/Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php 
+namespace AHT\Hello\Controller\Index;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+class Index extends \Magento\Framework\App\Action\Action{
+	protected $_request;
+	protected $_pageFactory;
+	public function __construct(Context $context, RequestInterface $request, PageFactory $pageFactory){
+		$this->_request=$request;
+		$this->_pageFactory=$pageFactory;
+		parent::__construct($context);
+	}
+	public function execute(){
+		/*echo "<h1>Hello Module - Index Controller - Index Action</h1>";
+		exit();*/
+		return $this->_pageFactory->create();
+	}
+	public function getInfo(){
+		$requestName=$this->_request->getFullActionName();
+		$result=explode("_",$requestName);
+		return $result;
+	}
+}
